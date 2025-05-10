efede3a5537aa6bcca7057d4d6e46754af0e7306
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-05-10 11:59:52 +0900

    wip

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -45,7 +45,7 @@ function lib_to_package_fixup_vendor_variants() {
     case "$1" in
         com.qualcomm.qti.dpm.api@1.0 | \
             vendor.qti.hardware.fm@1.0 | \
-            vendor.qti.imsrtpservice@3.0) | \
+            vendor.qti.imsrtpservice@3.0 | \
             libmmosal | \
             vendor.qti.diaghal@1.0 | \
             vendor.qti.hardware.qccsyshal@1.0 | \
